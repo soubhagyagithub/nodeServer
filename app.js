16ffe03429cc4756a9cd266230b677f96885dcf4
Soubhagya Ranjan Sethi <soubhagya.edu12@gmail.com>
2024-01-30 21:41:54 +0530

    added some endpoints

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res) =>{
-    console.log('Soubhagya Ranjan Sethi')
+    const url = req.url;
+
+    if (url === '/home'){
+      res.write('Welcome home');
+    }
+    else if (url === '/about'){
+        res.write('Welcome to About Us page');
+    }
+    else if (url ==='/node'){
+        res.write('Welcome to my Node Js project');
+    }else{
+        res.write('404 Not Found');
+    }
+    res.end();
 })
-server.listen(3000)
+server.listen(3000 , () =>{
+    console.log('server is listening on port 3000');
+})
