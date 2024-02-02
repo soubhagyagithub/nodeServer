5bc3e9f5606f5a7ec4237987a2eea5fdb22b3a5e
Soubhagya Ranjan Sethi <soubhagya.edu12@gmail.com>
2024-02-02 10:49:30 +0530

    added routes

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
 const app = express();
+
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title" > <input type="number" name="size"><button type="submit">add product</button></form>')
+app.use(adminRouter);
+app.use(shopRouter);
 
-    
-});
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+app.use((req, res, next) => {
+   res.status(404).send('<h1>Page not found!</h1>')
+   
 })
 
-app.use('/', (req, res, next) => {
-    res.send('<h1> hello to node js </h1>')
-});
 
 app.listen(3000, () =>{
     console.log('server is listiening on port 3000');
