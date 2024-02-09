ab1c29ebdc290842d4a7a740de8ac8ded7eb356b
Soubhagya Ranjan Sethi <soubhagya.edu12@gmail.com>
2024-02-09 20:13:45 +0530

    added crud operation

--- models/product.js
+++ models/product.js
@@ -1,37 +1,34 @@
-const fs = require('fs');
-const path = require('path');
 
-const p = path.join(
-    path.dirname(require.main.filename), 
-    'data', 
-    'products.json'
-);
-
-const getProductFromFile = cb => {
-    fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            cb([]);
-        } else {
-            return JSON.parse(fileContent);
-        }
-    });
-};
+const db = require('../util/database');
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
+    constructor(id, title, price, description, imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.price =price;
+        this.description = description;
+        this.imageUrl = imageUrl;
     }
 
     save() {
-        getProductFromFile(products => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
-        });
+        return db.execute('INSERT INTO products(title, price, description, imageUrl) VALUES( ?, ?, ?, ?)',
+        [this.title, this.price, this.description, this.imageUrl]
+        );
+     
+    }
+    static updateById(id, title, price, description, imageUrl) {
+        return db.execute('UPDATE products SET title = ?, price = ?, description = ?, imageUrl = ? WHERE id = ?',
+            [title, price, description, imageUrl, id]
+        );
     }
 
-    static fetchAll(cb) {
-        getProductFromFile(cb);
+    static fetchAll() {
+       return db.execute('SELECT * FROM products') ;
+    }
+    static findById(id){
+       return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
+    }
+    static deleteById(id){
+       db.execute('DELETE FROM products WHERE products.id = ?',[id])
     }
 };
