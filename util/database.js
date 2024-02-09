8f61e4ed64ddc8123f42fe3d11e3d6b14e5f3bb5
Soubhagya Ranjan Sethi <soubhagya.edu12@gmail.com>
2024-02-09 22:27:38 +0530

    used Sequelize package

--- util/database.js
+++ util/database.js
@@ -1,10 +1,9 @@
-const mySql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mySql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: ''
+const sequelize = new Sequelize('node_complete','root', 'Silu933@#1999', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
