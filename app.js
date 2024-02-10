3b0034883ccc93e613cfb5fb0be8a6aeb4ae744f
Soubhagya Ranjan Sethi <soubhagya.edu12@gmail.com>
2024-02-11 00:58:51 +0530

    Cart with many to many relationship

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const path = require('path');
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 const contactRouter = require('./routes/contact')
+const Product = require('./models/product');
+const User = require('./models/user');
+
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart_item')
 //imported sequelize
 const sequelize = require('./util/database');
     
@@ -25,9 +30,30 @@ app.use((req, res, next) => {
    
 })
 
-sequelize.sync()
-.then(result => {
-    // console.log(result);
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+
+sequelize
+// .sync({force: true})
+.sync()
+.then(result =>{
+    return User.findById(1);
+  
+})
+.then( user =>{
+    if (!user){
+     return User.create({name: 'Max', email: 'test@gmail.com'})
+    }
+})
+.then(user => {
+   return user.createCart(Cart);
+ 
+})
+.then( cart => {
     app.listen(3000, () =>{
         console.log('server is listiening on port 3000');
     })
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
 const Cart = require('../models/cart');
+const { decrypt } = require('dotenv');
 
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
@@ -12,12 +13,52 @@ exports.getProduct = (req, res, next) => {
     }))
 }
 
+exports.getIndex = (req, res, next) => {
+    
+    Product.fetchAll()
+    .then((result) => {
+     console.log(result);
+    })
+    .catch((err) => {
+    console.log(err);
+    })
+}
+
 
 exports.postCart = (req, res, next) => {
     const proId = req.body.productId;
-     
-    Product.findById(proId, (product) => {
-      Cart.addProduct(proId, product.price);
+    let fetchCart; 
+    req.user
+    .getCart()
+    .then(cart => {
+        fetchCart = cart;
+        return cart.getProduct({where: {id: proId}});
     })
-    res.redirect('/cart')
+    .then(products =>{
+        let product;
+        if (products.length > 0){
+            product = products[0]
+        }
+        let newQuantity = 1;
+        if (product){
+            //...
+        }
+        return Product.findById(proId)
+        .then(product => {
+            return fetchCart.addProduct(product, {through: { quantity: newQuantity}})
+        })
+    } )
+    .catch(err => console.log(err))
+}
+
+
+exports.getCart = (req, res, next) => {
+    console.log(req.user.cart);
+    req.user
+    .getCart()
+    .then(cart => {
+        return cart.getProduct(); 
+    })
+    .catch(err => console.log(err))
+
 }
